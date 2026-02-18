ea0f3a045876b01ff542caffd5556c0097f49565
Yohan-Helitha <ceylongroupcentral@gmail.com>
2026-02-18 18:10:17 +0530

    organize the code for visibility

--- app.js
+++ app.js
@@ -1,53 +1,74 @@
-async function myFunction() {
- try {
- const result = await myPromise;
- console.log(result);
- } catch (error) {
- console.log(error);
- }
-}
-myFunction();
-const myPromise = new Promise((resolve, reject) => {
- if (condition) {
- resolve('Success!');
- } else {
- reject('Failure!');
- }
-});
-myPromise.then((result) => {
- console.log(result);
-}).catch((error) => {
- console.log(error);
-});
-const myModule = require('./my-module.js');
-console.log(myModule.myFunction());
-const https = require('https');
-https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
- let data = '';
- resp.on('data', (chunk) => {
- data += chunk;
- });
- resp.on('end', () => {
- console.log(JSON.parse(data));
- });
-}).on('error', (err) => {
- console.log("Error: " + err.message);
-});
-const http = require('http');
-http.createServer(function (req, res) {
- res.writeHead(200, {'Content-Type': 'text/html'});
- res.write('Hello World!');
- res.end();
-}).listen(8080);
-
+// FILE SYSTEM
 const fs = require('fs');
 
-fs.readFile('file.txt', 'utf8', function (err, data) {
- if (err) throw err;
- console.log(data);
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+  if (err) throw err;
+  console.log('File saved!');
 });
 
-fs.writeFile('file.txt', 'Hello World!', function (err) {
- if (err) throw err;
- console.log('File saved!');
+fs.readFile('file.txt', 'utf8', function (err, data) {
+  if (err) throw err;
+  console.log(data);
 });
+
+
+// HTTP SERVER
+const http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8083);
+
+// HTTPS REQUEST
+const https = require('https');
+
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+  let data = '';
+
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  resp.on('end', () => {
+    console.log(JSON.parse(data));
+  });
+
+}).on('error', (err) => {
+  console.log("Error: " + err.message);
+});
+
+
+// MODULE
+const myModule = require('./my-module.js');
+console.log(myModule.myFunction());
+
+
+// PROMISE
+const condition = true;
+
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve('Success!');
+  } else {
+    reject('Failure!');
+  }
+});
+
+myPromise
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
+
+
+// ASYNC/AWAIT
+async function myFunction() {
+  try {
+    const result = await myPromise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+myFunction();
